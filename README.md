f46af3bc5c4f207279c918c3b84bc9bc167d796b
Kevin <xkerts@outlook.com>
2023-10-29 22:44:29 -0500

    Subida inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+# TASK LIST APP
+
+Create a React app that implements an input text field and a button to add an element to a list.
+
+Whenever the button is clicked, the value typed into the text field should be added to the list.
+
+Every list element must have a button that when pressed deletes the element from the list.
+
+# Use Typrescript
---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+
+import './App.css';
+type ItemId = `${string}-${string}-${string}-${string}-${string}` 
+interface Item { 
+  id: ItemId,
+  timestamp: number,
+  text: string
+}
+const INITIAL_ITEMS: Item[] = [
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: 'Hacer popo'
+  },
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: 'Hacer pipi'
+  }
+
+]
+
+function App() {
+  const [items, setItems] = useState(INITIAL_ITEMS);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {elements} = event.currentTarget;
+    const input = elements.namedItem('item');
+    const isInput = input instanceof HTMLInputElement;
+    if( !isInput || input == null) return
+
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      text: input.value
+    }
+
+    setItems((prevItems) => {
+      return [...prevItems, newItem];
+    })
+
+    input.value = '';
+
+  }
+
+  const createHandleRemoveItem = (id: ItemId) => () =>{
+    
+      setItems(prevItems =>{
+        return prevItems.filter(currentItem => currentItem.id !== id)
+      })
+
+  }
+
+  return (
+    <main>
+      <aside>
+        <h1>React Technical Exam</h1>
+        <h2>Add and remove elements from a list</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Elemento a introducir:
+            <input name='item' required type='text' placeholder='Type something...'/>
+          </label>
+          <button>Add Element</button>
+        </form>
+      </aside>
+      <section>
+        <h2>List of elements</h2>
+        <ul>
+          {
+            items.map( item => {
+              return (
+                <li key={item.id}>
+                  {item.text}
+                  <button onClick={createHandleRemoveItem(item.id)}>Delete</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+    </main>
+  )
+}
+
+export default App
